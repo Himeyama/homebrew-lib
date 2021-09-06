1700333fffb756273a1c2b99444a237be93dbf30
hikari <39254183+Himeyama@users.noreply.github.com>
2021-09-06 16:38:50 +0900

    Update octave.rb

--- octave.rb
+++ octave.rb
@@ -15,7 +15,6 @@ class Octave < Formula
   depends_on "openblas"
   depends_on "pcre"
   depends_on "readline"
-  depends_on "arpack"
   depends_on "fltk"
   depends_on "fontconfig"
   depends_on "freetype"
